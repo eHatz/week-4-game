1efb1cd8817bd59169730cb015871bcf07f32dc5
Efthemi Hatzis <ehatz01@gmail.com>
2016-06-07 17:11:16 -0400

    finished hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,22 +16,27 @@ var char3 = {
 }
 var char4 = {
 	name: "Darth Maul",
-	health: 100,
+	health: 180,
 	damage: 25
 }
+
+document.querySelector('#character1 .HP').innerHTML = char1.health;
+document.querySelector('#character2 .HP').innerHTML = char2.health;
+document.querySelector('#character3 .HP').innerHTML = char3.health;
+document.querySelector('#character4 .HP').innerHTML = char4.health;
+
 var charStats = [char1, char2, char3, char4]; // array of the character stats
 var charactersArr = $('.character'); //array of original character divs
 var enemiesArr;
 
 var pickAttacker; // temporarily stores value of attacker
 var attackChar; // hold value of chosen character to attack with, starts undefined and when a value is entered it stops the for loop for continuing
-var attackHp;
-var attackDmg;
+var attackDmg = 0;
 
 var pickDefender; // temporarily stores value of defender
 var defendChar; // hold value of chosen defender, starts undefined and when a value is entered it stops the for loop for continuing
-var defendHp;
 var defendDmg;
+
 $( document ).ready(function() {
 
 	$('.character').on('click', function() { // select a div that contains characters/images and puts it in the attacker section
@@ -47,8 +52,6 @@ $( document ).ready(function() {
 			}
 		}
 		attackChar = pickAttacker; // attackChar becomes defined
-		attackHp = charStats[charactersArr.index(attackChar)].health;// health lsited in object
-		attackDmg = charStats[charactersArr.index(attackChar)].damage; //damage listed in object
 		enemiesArr = $('.enemies');// array of enemy divs
 	});
 
@@ -63,28 +66,59 @@ $( document ).ready(function() {
 			}
 		}
 		defendChar = pickDefender; // defendChar becomes defined
-		defendHp = charStats[charactersArr.index(defendChar)].health; // health listed in object
 		defendDmg = charStats[charactersArr.index(defendChar)].damage; // damage listed in object
-		document.querySelector('#messages').innerHTML = ""; //resets the message typed below when a character enters the defend section
+		document.querySelector('#message1').innerHTML = ""; //resets the message typed below when a character enters the defend section
+		document.querySelector('#message2').innerHTML = ""; //resets the message typed below when a character enters the defend section
+
 	});
-		
+
+
 	$('.attackBtn').on('click', function() {
 
-		if (defendChar !== undefined && defendHp > 0) {
-			defendHp = defendHp - attackDmg;
-			attackHp = attackHp - defendDmg;
-			document.querySelector('.currentEnemy .HP').innerHTML = defendHp;
-			document.querySelector('#messages').innerHTML = "You dealt " + attackDmg + " to the enemy" + "<br />" + "The enemy attacked you for " + defendDmg + " HP" ;
+		if (defendChar === undefined){
+			document.querySelector('#message1').innerHTML = "No enemy has been selected"; // if a character was not put into the defender section displays this message
+			document.querySelector('#message2').innerHTML = "";
+		} else{
 
-			attackDmg = attackDmg + charStats[charactersArr.index(attackChar)].damage; // adds the base damage of the attacker ever time button is clicked
+			if (defendChar !== undefined && document.querySelector('#yourChar .character .HP').innerHTML > 0) { //if your chatacters health is above 0
 
-		} else if (defendHp <= 0){
-			document.querySelector('#messages').innerHTML = "You have defeated the enemy";
-			defendChar = undefined;
-			$('.currentEnemy').remove();
-		}else {
-			document.querySelector('#messages').innerHTML = "No enemy has been selected"; // if a character was not put into the defender section displays this message
+				attackDmg = attackDmg + charStats[charactersArr.index(attackChar)].damage; // adds the base damage of the attacker ever time button is clicked
+				document.querySelector('.currentEnemy .HP').innerHTML = document.querySelector('.currentEnemy .HP').innerHTML - attackDmg;
+				document.querySelector('#message1').innerHTML = "You dealt " + attackDmg + " to the enemy";
+
+			} 
+			if (document.querySelector('.currentEnemy .HP').innerHTML > 0) {
+				document.querySelector('#yourChar .character .HP').innerHTML = document.querySelector('#yourChar .character .HP').innerHTML - defendDmg;
+				document.querySelector('#message2').innerHTML = "The enemy attacked you for " + defendDmg + " HP" ;
+
+			} else if (document.querySelector('.currentEnemy .HP').innerHTML <= 0){
+				document.querySelector('#message1').innerHTML = "You have defeated the enemy";
+				defendChar = undefined;
+				$('.currentEnemy').remove();
+			}			
+
+			if (document.querySelector('#yourChar .character .HP').innerHTML <= 0){ //if your chars health is 0 and below
+				document.querySelector('#message1').innerHTML = "You were defeated. Reset to try again?";
+				document.querySelector('#message2').innerHTML = "";
+				$('#message2').append($('<input type="button" class="btn btn-danger attackBtn" id="reset" value="Reset">'));
+				$('#reset').click(function() {
+					location.reload();
+				});
+			}
+
+
+			if ((document.querySelector('#enemiesAvailable').innerHTML).length === 4 && defendChar === undefined && attackChar !== undefined) {
+				document.querySelector('#message1').innerHTML = "You Won!! Reset to play again!";
+				document.querySelector('#message2').innerHTML = "";
+				$('#message2').append($('<input type="button" class="btn btn-success attackBtn" id="reset" value="Reset">'));
+				$('#reset').click(function() {
+					location.reload();
+				});
+
+			}
 		}
+
+
 	});
 
 });
